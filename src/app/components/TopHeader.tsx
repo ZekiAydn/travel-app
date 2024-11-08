17c6465c2e394b945ee7eebb8b8b5f360488d8da
Zeki Aydın <105093184+ZekiAydn@users.noreply.github.com>
2024-11-08 10:32:35 +0300

    responsive header style

--- src/app/components/TopHeader.tsx
+++ src/app/components/TopHeader.tsx
@@ -10,7 +10,7 @@ const TopHeader: React.FC = () => {
     const { t } = useLanguageStore(); // t fonksiyonunu kullanarak çevirileri alıyoruz
 
     return (
-        <div className="flex flex-col space-y-2 bg-gray-100 py-2 border-b border-gray-200">
+        <div className="responsive-header flex flex-col space-y-2 bg-gray-100 py-2 border-b border-gray-200">
             <div className="container mx-auto flex justify-end items-center px-4 text-gray-600 text-sm">
 
                 <div className="hidden md:flex items-center space-x-8">
